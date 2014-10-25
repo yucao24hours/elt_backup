ddefd83d6862b91a66c7708e1d6924356c30a022
Yuka Kato <y-kato@esm.co.jp>
2014-10-25 22:30:14 +0900

    Remove past schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,14 +1,4 @@
 ActiveRecord::Schema.define(version: 20141025085130) do
-
-  create_table "lts", force: true do |t|
-    t.string   "title",       null: false
-    t.integer  "attendee_id", null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "lts", ["attendee_id"], name: "index_lts_on_attendee_id", unique: true
-
   create_table "votes", force: true do |t|
     t.string   "target_type"
     t.integer  "attendee_id"

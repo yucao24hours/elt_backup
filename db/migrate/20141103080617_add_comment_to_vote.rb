f45f6d7f1318574a44c8794a8fe24d0e00111e92
Yuka Kato <y-kato@esm.co.jp>
2014-11-17 00:22:02 +0900

    Use change method

--- db/migrate/20141103080617_add_comment_to_vote.rb
+++ db/migrate/20141103080617_add_comment_to_vote.rb
@@ -1,9 +1,5 @@
 class AddCommentToVote < ActiveRecord::Migration
-  def up
+  def change
     add_column :votes, :comment, :string
-  end
-
-  def down
-    remove_column :votes, :comment
   end
 end

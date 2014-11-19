451832f0d33bf5d3f9946418f82394061605d890
Yuka Kato <y-kato@esm.co.jp>
2014-11-19 21:46:56 +0900

    Add memo

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,6 +1,7 @@
 class ResultsController < ApplicationController
   def index
     #TODO あとでなおす
+    #すべての vote インスタンスを、vote_target_id ごとにグルーピングして、[得票数, vote_targetインスタンス] の形を配列にして、target の個数分だけ要素をつくる。
     @results = Vote.all
   end
 

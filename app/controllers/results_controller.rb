1211ddacbac7760bca7b8387fefa0170464deb2a
Yuka Kato <y-kato@esm.co.jp>
2014-11-21 00:18:44 +0900

    Show result title

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,6 +2,7 @@ class ResultsController < ApplicationController
   def index
     v = Vote.vote_to(params[:target_type])
     @votes = v.group_by {|vote| vote.vote_target_id}
+    @type  = params[:target_type]
   end
 
   def show

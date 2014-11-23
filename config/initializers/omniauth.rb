643ee2e347217d1d27a99ea3fe8ca29997a5127e
Yuka Kato <y-kato@esm.co.jp>
2014-11-23 11:44:46 +0900

    Login with Twitter

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  provider :github,  ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  provider :twitter, ENV['TWITTER_CONSUMER_KEY'], ENV['TWITTER_CONSUMER_SECRET']
 end
